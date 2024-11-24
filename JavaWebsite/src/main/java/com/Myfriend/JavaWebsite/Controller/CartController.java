9f4eada82938345e5f535e6e042bde3cd27f43fe
NgHoangHaoTam <haotamtkp@gmail.com>
2024-11-25 01:44:25 +0700

    Update CartController.java

--- JavaWebsite/src/main/java/com/Myfriend/JavaWebsite/Controller/CartController.java
+++ JavaWebsite/src/main/java/com/Myfriend/JavaWebsite/Controller/CartController.java
@@ -3,21 +3,29 @@ package com.Myfriend.JavaWebsite.Controller;
 import com.Myfriend.JavaWebsite.Entity.Cart;
 import com.Myfriend.JavaWebsite.Service.CartService;
 import com.Myfriend.JavaWebsite.payload.ReponseData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
     private CartService cartService;
+
+    // Sử dụng constructor injection
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 
     @GetMapping("/{cart_id}/my_cart")
     public ResponseEntity<?> getCart(@PathVariable Integer cart_id) {
-        // Lấy thông tin giỏ hàng bằng id
         Cart cart = cartService.getCartById(cart_id);
         ReponseData responseData = new ReponseData();
 
@@ -30,21 +38,20 @@ public class CartController {
         }
     }
 
-
-    @DeleteMapping("/{id}/clear")
-    public ResponseEntity<?> clearCart(@PathVariable Integer id,@RequestBody Cart cart) {
-
-        cartService.ClearCartById(id);
-        return new ResponseEntity<>(cart, HttpStatus.OK);
-
+    @DeleteMapping("/{cart_id}/clear")
+    public ResponseEntity<?> clearCart(@PathVariable Integer cart_id) {
+        ReponseData responseData = new ReponseData();
+        cartService.ClearCartById(cart_id);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}/cart/total_price")
-    public ResponseEntity<?> getTotalAmount(@PathVariable Integer id) {
 
-        BigDecimal totalPrice = cartService.getCartTotal(id);
-
+    @GetMapping("{cart_id}/cart/total_price")
+    public ResponseEntity<?> getTotalAmount(@PathVariable Integer cart_id) {
+        System.out.println("Error" + cart_id);
+        BigDecimal totalPrice = cartService.getCartTotal(cart_id);
         return new ResponseEntity<>(totalPrice, HttpStatus.OK);
     }
 
 }
+
