b941a3000feebce10195a9bd3e1c804606bcb123
VoTaDuy <154273585+VoTaDuy@users.noreply.github.com>
2024-11-29 14:52:18 +0700

    Delete JavaWebsite directory

--- JavaWebsite/src/main/java/com/Myfriend/JavaWebsite/Repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Myfriend.JavaWebsite.Repository;
-
-import com.Myfriend.JavaWebsite.Entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-
-}
--- JavaWebsite/src/main/java/com/Myfriend/JavaWebsite/Service/Imp/FileServiceImp.java
+++
@@ -1,9 +0,0 @@
-package com.Myfriend.JavaWebsite.Service.Imp;
-
-import org.springframework.core.io.Resource;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileServiceImp {
-    boolean saveFile(MultipartFile file);
-    Resource loadFile(String filename);
-}
